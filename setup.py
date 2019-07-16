2780b3b17c9410d2653779d41345d32121c74c7e
Brian Dodge <brian.dodge@dvla.gov.uk>
2019-07-16 07:11:41 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='gits',
     version='0.0.1',
     description='Multi-module wrapper for git',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/briangarydodge/utilities-gits',
     author='Brian Dodge',
     author_email='briangarydodge@gmail.com',
+    packages=setuptools.find_packages(),
     install_requires=[
         'pyyaml'
     ],
